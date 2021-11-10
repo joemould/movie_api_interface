7baf76d637db5559e8a8bc8f7c3315f92585a925
joemould <mould_joe@hotmail.com>
2021-11-10 23:50:55 +0000

    improves structure and utilises os for filepath

--- connect.py
+++ connect.py
@@ -1,51 +1,58 @@
 import sys
+import os
 import requests
 import pandas as pd
 
-# should be allowed to specify movie search string in cli and this does the rest for you
-# build functionality when required not for the sake of it
+
+BASE_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 api_key = "f8b8c7ff9bdb10d377dbf69f1d19dfce"
 base_url = "https://api.themoviedb.org/3"
 
-def run(movie_query):
+
+def get_movies_by_searchstr(query):
     movie_search_endpoint = "/search/movie"
-    movie_search_url =  "{}{}?api_key={}&query={}".format(base_url, movie_search_endpoint, api_key, movie_query)
+    movie_search_url =  "{}{}?api_key={}&query={}".format(base_url, movie_search_endpoint, api_key, query)
 
     r = requests.get(movie_search_url)
+    if r.status_code not in range(200,299):
+        r.raise_for_status()
 
-    if r.status_code in range(200,299):
-        data = r.json()
-        results = data['results']
-        if len(results) > 0:
-            movie_ids = set()
-            for result in results:
-                _id = result['id']
-                print(result['title'], _id)
-                movie_ids.add(_id)
-            print(list(movie_ids))
+    results = r.json()['results']
+    if not len(results):
+        print("No results from search query={}.".format(query))
+        return
+    movie_ids = set()
+    for result in results:
+        _id = result['id']
+        movie_ids.add(_id)
 
-    output = 'movies.csv'
     movie_data = []
-
     for movie_id in movie_ids:
         movie_by_id_endpoint = "/movie/{}".format(movie_id)
+        # TODO create a function to construct url including kwargs to be reused at top of run
         movie_by_id_url = "{}{}?api_key={}".format(base_url, movie_by_id_endpoint, api_key)
         r = requests.get(movie_by_id_url)
-        print(r.status_code)
-        print(r.text)
         if r.status_code in range(200,299):
             movie_data.append(r.json())
 
-    # TODO combine data from multiple sources - web scraping tool
+    if not len(movie_data):
+        print("No movie data found for movie_ids {}.".format(movie_ids))
+        return
 
+    # TODO combine data from multiple sources - web scraping tool
+    os.makedirs(DATA_DIR, exist_ok=True)
+    output_filename = "{}-movies.csv".format(query)
+    output_filepath = os.path.join(DATA_DIR, output_filename)
+    print("Saving csv to {}".format(output_filepath))
     df = pd.DataFrame(movie_data)
     print(df.head())
-    df.to_csv(output, index=False)
+    df.to_csv(output_filepath, index=False)
 
 
 if __name__ == '__main__':
     # TODO use python click to parse args and validate
     assert len(sys.argv) == 2
     movie_query = sys.argv[1]
-    run(movie_query)
+    get_movies_by_searchstr(movie_query)
