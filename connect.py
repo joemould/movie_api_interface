fe436e33dbc7f1cf50869eb7a6d25797fabe9779
joemould <mould_joe@hotmail.com>
2021-11-10 23:02:06 +0000

    creates run fnc which takes query parameter as cli arg

--- connect.py
+++ connect.py
@@ -1,55 +1,51 @@
+import sys
 import requests
 import pandas as pd
 
-api_key_v3 = "f8b8c7ff9bdb10d377dbf69f1d19dfce"
-# api_key_v4 = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGI4YzdmZjliZGIxMGQzNzdkYmY2OWYxZDE5ZGZjZSIsInN1YiI6IjYxOGJlZGJlY2I2ZGI1MDA4ZDYwYzFhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qtxKp38FOFPUVxArqJYl9M7JLwNq8zT5eDuqsIkH3m4"
+# should be allowed to specify movie search string in cli and this does the rest for you
+# build functionality when required not for the sake of it
 
-api_version = 3
-api_base_url = "https://api.themoviedb.org/{}".format(api_version)
+api_key = "f8b8c7ff9bdb10d377dbf69f1d19dfce"
+base_url = "https://api.themoviedb.org/3"
 
-movie_id = 503
-movie_by_id_endpoint = "/movie/{}".format(movie_id)
-movie_by_id_url = "{}{}?api_key={}".format(api_base_url, movie_by_id_endpoint, api_key_v3) # v3
-# movie_by_id_url = "{}{}".format(api_base_url, movie_endpoint) # v4
+def run(movie_query):
+    movie_search_endpoint = "/search/movie"
+    movie_search_url =  "{}{}?api_key={}&query={}".format(base_url, movie_search_endpoint, api_key, movie_query)
 
-movie_search_endpoint = "/search/movie"
-movie_query = "Fight"
-movie_search_url =  "{}{}?api_key={}&query={}".format(api_base_url, movie_search_endpoint, api_key_v3, movie_query) # v3
+    r = requests.get(movie_search_url)
 
-# headers = {
-#     'Authorization': 'Bearer {}'.format(api_key_v4),
-#     'Content-Type': 'application/json;charset=utf-8'
-# } # v4
-
-
-r = requests.get(movie_search_url) # v3
-# r = requests.get(movie_url, headers=headers) # v4
-
-if r.status_code in range(200,299):
-    data = r.json()
-    results = data['results']
-    if len(results) > 0:
-        movie_ids = set()
-        for result in results:
-            _id = result['id']
-            print(result['title'], _id)
-            movie_ids.add(_id)
-        print(list(movie_ids))
-
-output = 'movies.csv'
-movie_data = []
-
-for movie_id in movie_ids:
-    movie_by_id_endpoint = "/movie/{}".format(movie_id)
-    movie_by_id_url = "{}{}?api_key={}".format(api_base_url, movie_by_id_endpoint, api_key_v3)
-    r = requests.get(movie_by_id_url)
-    print(r.status_code)
-    print(r.text)
     if r.status_code in range(200,299):
-        movie_data.append(r.json())
+        data = r.json()
+        results = data['results']
+        if len(results) > 0:
+            movie_ids = set()
+            for result in results:
+                _id = result['id']
+                print(result['title'], _id)
+                movie_ids.add(_id)
+            print(list(movie_ids))
 
-# TODO combine data from multiple sources - web scraping tool
+    output = 'movies.csv'
+    movie_data = []
 
-df = pd.DataFrame(movie_data)
-print(df.head())
-df.to_csv(output, index=False)
+    for movie_id in movie_ids:
+        movie_by_id_endpoint = "/movie/{}".format(movie_id)
+        movie_by_id_url = "{}{}?api_key={}".format(base_url, movie_by_id_endpoint, api_key)
+        r = requests.get(movie_by_id_url)
+        print(r.status_code)
+        print(r.text)
+        if r.status_code in range(200,299):
+            movie_data.append(r.json())
+
+    # TODO combine data from multiple sources - web scraping tool
+
+    df = pd.DataFrame(movie_data)
+    print(df.head())
+    df.to_csv(output, index=False)
+
+
+if __name__ == '__main__':
+    # TODO use python click to parse args and validate
+    assert len(sys.argv) == 2
+    movie_query = sys.argv[1]
+    run(movie_query)
